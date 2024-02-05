bbe8bde5c69b3fd2265eb493a84f233302a28cde
egydyahermawan <egy20ti@mahasiswa.pcr.ac.id>
2024-02-05 12:16:13 +0700

    bismillah fixed si migration jeung seeder na

--- database/seeders/MentorSeeder.php
+++ database/seeders/MentorSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Mentor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Crypt;
 
 class MentorSeeder extends Seeder
 {
@@ -15,8 +16,8 @@ class MentorSeeder extends Seeder
     {
         Mentor::create([
             'fullname' => 'Egy Dya Hermawan',
-            'email' => 'egydya.edh12@gmail.com',
-            'password' => 'a17b616514c29883763f21b409e403e471ec12d21d65a6131ed53dbc869a4a48',
+            'email' => 'egy@gmail.com',
+            'password' => Crypt::encryptString('egy123'),
             'phone' => '082387655402',
             'skill' => 'Web Fullstack, UI/UX Design',
             'status' => 'Active',
